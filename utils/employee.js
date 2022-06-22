ec718cce58ff05bce9625eeaa8cc2c1ab8e27c0e
DPLong <dplong742@gmail.com>
2022-06-21 22:55:57 -0400

    employee.js class constructor

---
+++ utils/employee.js
@@ -0,0 +1,26 @@
+//employee class constructor
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email 
+    }
+
+    //get name input
+    getName () {
+        return this.name;
+    }
+
+    //get id input
+    getId () {
+        return this.id;
+    }   
+
+    //get email input
+    getEmail () {
+        return this.email;
+    }
+};
+
+//module export
+module.exports = { Employee };
